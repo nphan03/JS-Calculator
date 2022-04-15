b3b03e54ef53a2b0a93aea28a4df3dbe4fddf8ec
Alice Phan <nphan03.145@gmail.com>
2022-04-14 19:47:54 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+'use strict';
+function add(a,b){
+    return a + b;
+}
+function substract(a,b){
+    return a-b;
+}
+function multiply(a,b){
+    return a*b;
+}
+function divide(a,b){
+    return a/b;
+}
+
+function operate (operator, a, b){
+    switch (operator) {
+        case '+':
+            return add(a,b);
+        case '-':
+            return substract(a,b);
+        case '*':
+            return multiply(a,b);
+        default:
+            return divide(a,b);
+
+    }
+}
